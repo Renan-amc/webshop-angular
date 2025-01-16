295d1078e1e7a87a88dce950fa01d6895260427c
Renan Couto <renanmv10@hotmail.com>
2025-01-16 11:38:26 -0300

    Fazendo a exibicao e associacao do component detalhes produto com a base de dados dos produtos

--- src/app/produtos/detalhes-produto/detalhes-produto.component.ts
+++ src/app/produtos/detalhes-produto/detalhes-produto.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Pipe } from '@angular/core';
+import { ProdutosService } from '../../produtos.service';
+import { IProduto } from '../../produtos';
+import { ActivatedRoute } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-detalhes-produto',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './detalhes-produto.component.html',
   styleUrl: './detalhes-produto.component.css'
 })
 export class DetalhesProdutoComponent {
+
+  produto: IProduto | undefined;
+  quantidade = 1;
+
+  constructor(
+    private produtosService: ProdutosService, 
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void { 
+    const routeParams = this.route.snapshot.paramMap;
+    const produtoId = Number(routeParams.get("id"));
+    this.produto = this.produtosService.getOne(produtoId);
+   }
+
+
+
 
 }
--- src/app/produtos/produtos.component.ts
+++ src/app/produtos/produtos.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
 import { IProduto, produtos } from '../produtos';
+import { ProdutosService } from '../produtos.service';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-produtos',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './produtos.component.html',
   styleUrl: './produtos.component.css'
 })
 export class ProdutosComponent {
 
-  produto: IProduto[]  = produtos;
+  produtos: IProduto[]  | undefined;
+
+  constructor(private produtosService: ProdutosService) {
+  }
+
+  ngOnInit(): void {
+    this.produtos = this.produtosService.getAll();
+  }
 }
