26190fb999dc6b5be2b6b9b27e9780d1a9faa383
Renan Couto <renanmv10@hotmail.com>
2025-01-16 10:19:07 -0300

    Criando pagina nao encontrada pra rotas sem destino

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ProdutosComponent } from './produtos/produtos.component';
+import { NaoEncontradaComponent } from './nao-encontrada/nao-encontrada.component';
 
 export const routes: Routes = [
     {
@@ -18,5 +19,15 @@ export const routes: Routes = [
         path: 'produtos',
         component: ProdutosComponent,
         title: 'Produtos!'
+    },
+    {
+        path: '',
+        component: ProdutosComponent,
+        title: 'Produtos!'
+    },
+    {
+        path: '**',
+        component: NaoEncontradaComponent,
+        title: 'Pagina não encontrada!'
     }
 ];
---
+++ src/app/nao-encontrada/nao-encontrada.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nao-encontrada',
+  imports: [],
+  templateUrl: './nao-encontrada.component.html',
+  styleUrl: './nao-encontrada.component.css'
+})
+export class NaoEncontradaComponent {
+
+}
