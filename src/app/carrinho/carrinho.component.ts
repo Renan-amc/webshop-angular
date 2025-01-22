62ab010704d10a4f0e966e279d5b54b309fb8a4b
Renan Couto <renanmv10@hotmail.com>
2025-01-22 12:44:04 -0300

    Ajustes nas funcionalidades do carrinho

--- src/app/carrinho/carrinho.component.ts
+++ src/app/carrinho/carrinho.component.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import { CarrinhoService } from '../carrinho.service';
+import { IProdutoCarrinho } from '../produtos';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-carrinho',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './carrinho.component.html',
   styleUrl: './carrinho.component.css'
 })
 export class CarrinhoComponent {
+  itensCarrinho: IProdutoCarrinho[] = [];
+  total: number = 0;
+
+  constructor(
+    public carrinhoService: CarrinhoService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.itensCarrinho = this.carrinhoService.obtemCarrinho();
+    this.calculoTotal();
+  }
+
+  removeProdutoCarrinho(produtoId: number) {
+    this.itensCarrinho = this.itensCarrinho.filter(item => item.id !== produtoId);
+    this.carrinhoService.removerProdutoCarrinho(produtoId);
+    this.calculoTotal();
+  }
+
+  calculoTotal() {
+    this.total = this.itensCarrinho.reduce((prev,curr) => prev + curr.preco * curr.quantidade, 0);
+  }
+
+  comprar() {
+    alert("Parabéns voce finalizou sua compra!");
+    this.carrinhoService.limparCarrinho();
+    this.router.navigate(["produtos"]);
+  }
 
 }
