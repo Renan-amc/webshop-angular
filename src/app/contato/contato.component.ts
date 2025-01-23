fcc7029c1c5a2ef0308513cd125072d517f76755
Renan Couto <renanmv10@hotmail.com>
2025-01-23 14:38:29 -0300

    Criando formulario e toda sua logica

--- src/app/contato/contato.component.ts
+++ src/app/contato/contato.component.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgxMaskDirective, NgxMaskPipe } from 'ngx-mask';
 
 @Component({
   selector: 'app-contato',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, NgxMaskDirective],
   templateUrl: './contato.component.html',
   styleUrl: './contato.component.css'
 })
-export class ContatoComponent {
+export class ContatoComponent implements OnInit{
 
+  
+  formContato: any;
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+   ngOnInit(): void {
+
+    this.formContato = this.fb.group({
+      nome:["",[
+        Validators.minLength(4),
+        Validators.required
+        ]],
+      assunto: ["",[
+        Validators.minLength(10),
+        Validators.required
+        ]],
+      telefone: ["",[
+        Validators.minLength(11),
+        Validators.required
+        ]],
+      email: ["",[
+        Validators.email,
+        Validators.required
+        ]],
+      mensagem: ["",[
+        Validators.minLength(20),
+        Validators.required
+        ]]
+    });
+   }
+
+   enviarFormulario() {
+    alert("A mensagem foi enviada");
+    this.formContato.reset();
+   }
 }
